9ae1123a91fbc5b10df89ce8cbca0f44925ee62c
Guilherme Segatto <segattooguilherme@gmail.com>
2024-12-26 17:51:33 -0300

    familySection

--- src/components/organism/FamilySection.tsx
+++ src/components/organism/FamilySection.tsx
@@ -8,7 +8,7 @@ const FamilySection = () => {
       alt="Mapa do Brasil"
       width={1440}
       height={780}
-      className="object-contain lg:mt-[-180px] mt-[-100px] md:mt-[-260px] w-full max-w-6xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl"
+      className="object-contain lg:mt-[-180px] mt-[-110px] md:mt-[-260px] w-full max-w-6xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl"
     />
   </div>
   )

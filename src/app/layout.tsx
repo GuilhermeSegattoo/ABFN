af39b93b65a49eb526c6682fed6162a05b6803b8
Guilherme Segatto <segattooguilherme@gmail.com>
2024-12-29 13:35:36 -0300

    ajuste 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="font-poppins">
         <NavBar />
         {children}
+        <Analytics />
       </body>
     </html>
   );

c0775595929b6dc8a8fad27ca24365dcbd5d5bd5
Guilherme Segatto <segattooguilherme@gmail.com>
2024-12-31 10:23:10 -0300

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+// next-sitemap.js
+module.exports = {
+    siteUrl: 'https://www.abfnumerosas.org', // Substitua com o seu domínio
+    generateRobotsTxt: true, // Gera o arquivo robots.txt automaticamente
+    changefreq: 'daily', // Frequência de atualização
+    priority: 0.7, // Prioridade das páginas
+    sitemapSize: 5000, // Número máximo de URLs por arquivo
+  };
+  

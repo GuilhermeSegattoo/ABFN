02aadc14443e34f5894b1222dd674b9e79783d9b
Guilherme Segatto <segattooguilherme@gmail.com>
2024-12-31 12:12:29 -0300

    ajuste 10 metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,30 @@
 import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
-  title: "ABFN",
-  description: "Associação Brasileira de Famílias Numerosas",
+  title: "Associação Brasileira de Famílias Numerosas | ABFN",
+  description: "A ABFN é uma associação que luta pelos direitos das famílias numerosas no Brasil, oferecendo apoio jurídico, educacional e promovendo políticas públicas.",
+  keywords: "Associação Brasileira de Famílias Numerosas, ABFN, famílias numerosas, direitos das famílias, apoio familiar, políticas públicas para famílias",
+  author: "Associação Brasileira de Famílias Numerosas",
+  og: {
+    title: "Associação Brasileira de Famílias Numerosas | ABFN",
+    description: "A ABFN oferece apoio jurídico e social para famílias numerosas, com ações e políticas públicas para melhorar a qualidade de vida dessas famílias.",
+    image: "/og-image.jpg", // Substitua por uma imagem representativa do seu site
+    url: "https://www.abfnumerosas.org",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Associação Brasileira de Famílias Numerosas | ABFN",
+    description: "A ABFN defende os direitos das famílias numerosas, oferecendo apoio jurídico e promovendo políticas públicas.",
+    image: "/twitter-image.jpg", // Substitua por uma imagem otimizada para Twitter
+  },
   icons: {
     icon: "/lg.svg", // Caminho do arquivo favicon
     shortcut: "/lg.svg", // Atalho do ícone (opcional)
     apple: "/lg.svg", // Ícone específico para dispositivos Apple (opcional)
   },
 };
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (

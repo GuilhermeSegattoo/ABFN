d268b81c7a7253e2c4eac067e0201863ef70fbff
Guilherme Segatto <segattooguilherme@gmail.com>
2025-01-22 14:14:38 -0300

    Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,25 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.abfnumerosas.org', //  URL do seu site
-    generateRobotsTxt: false, // Gera automaticamente o robots.txt
-    changefreq: 'daily', // Frequência de atualização sugerida
-    priority: 0.7, // Prioridade padrão
-    sitemapSize: 5000, // Limite de URLs por arquivo de sitemap
-    exclude: ['/admin/*', '/secret/*'], // Exclua rotas específicas
-    alternateRefs: [
-      // Exemplo para rotas multilíngues
-      {
-        href: 'https://www.abfnumerosas.org/en',
-        hreflang: 'en',
-      },
-      {
-        href: 'https://www.abfnumerosas.org/pt',
-        hreflang: 'pt',
-      },
-      {
-        href: 'https://www.abfnumerosas.org/es',
-        hreflang: 'es',
-      },
-    ],
-  };
-  
+  siteUrl: 'https://www.abfnumerosas.org', // URL do seu site
+  generateRobotsTxt: true, // Gera automaticamente o robots.txt
+  changefreq: 'daily', // Frequência de atualização sugerida
+  priority: 0.7, // Prioridade padrão
+  sitemapSize: 5000, // Gera apenas um arquivo, já que seu site tem menos de 5000 URLs
+  generateIndexSitemap: false, // Desativa o índice de sitemaps (apenas um sitemap será gerado)
+  exclude: [], // Exclua rotas específicas se necessário
+  alternateRefs: [
+    {
+      href: 'https://www.abfnumerosas.org/en',
+      hreflang: 'en',
+    },
+    {
+      href: 'https://www.abfnumerosas.org/pt',
+      hreflang: 'pt',
+    },
+    {
+      href: 'https://www.abfnumerosas.org/es',
+      hreflang: 'es',
+    },
+  ],
+};
